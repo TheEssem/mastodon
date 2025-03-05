1a9786c2fed14f0b9291a763fafd5252ae220a43
Essem <smswessem@gmail.com>
2025-03-05 12:47:48 -0600

    Add list of reactions

--- db/schema.rb
+++ db/schema.rb
@@ -1117,6 +1117,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_100506) do
     t.bigint "favourites_count", default: 0, null: false
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.bigint "reactions_count", default: 0, null: false
     t.bigint "untrusted_favourites_count"
     t.bigint "untrusted_reblogs_count"
     t.bigint "quotes_count", default: 0, null: false
