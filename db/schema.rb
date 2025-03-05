03ec984b21b8580024d4e659c268d94ab6f32d24
Essem <smswessem@gmail.com>
2025-03-05 01:14:37 -0600

    Fix schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_29_144813) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_05_023754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
