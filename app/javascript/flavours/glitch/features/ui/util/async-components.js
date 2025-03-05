f542123a7a8abd1563d328a79ee5ddd05f6850ea
Essem <smswessem@gmail.com>
2025-03-05 12:47:48 -0600

    Add list of reactions

--- app/javascript/flavours/glitch/features/ui/util/async-components.js
+++ app/javascript/flavours/glitch/features/ui/util/async-components.js
@@ -86,6 +86,10 @@ export function Favourites () {
   return import('../../favourites');
 }
 
+export function Reactions () {
+  return import(/* webpackChunkName: "flavours/glitch/async/reactions" */'../../reactions');
+}
+
 export function FollowRequests () {
   return import('../../follow_requests');
 }
